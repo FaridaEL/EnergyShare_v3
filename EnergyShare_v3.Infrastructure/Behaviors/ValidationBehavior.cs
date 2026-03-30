294e973562bab6d699ce4a8a8cb122d6f1ca2293
Farida EL KHABBABI <PSR12762@students.ephec.be>
2026-03-30 07:27:11 +0200

    ajout des pipeline behaviors (logging, validation, transaction, UoW)

---
+++ EnergyShare_v3.Infrastructure/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergyShare_v3.Infrastructure.Behaviors
+{
+    public class ValidationBehavior<TMessage, TResponse>(
+    IEnumerable<IValidator<TMessage>> validators)
+    : IPipelineBehavior<TMessage, TResponse>
+    where TMessage : IMessage
+    {
+        public async ValueTask<TResponse> Handle(
+            TMessage message,
+            MessageHandlerDelegate<TMessage, TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            if (validators.Any())
+            {
+                var context = new ValidationContext<TMessage>(message);
+
+                var validationResults = await Task.WhenAll(
+                    validators.Select(v =>
+                        v.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResults
+                    .SelectMany(r => r.Errors)
+                    .Where(f => f != null)
+                    .ToList();
+
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+
+            return await next(message, cancellationToken);
+        }
+    }
+}
---
+++ EnergyShare_v3.Infrastructure/Helpers/TypeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Mediator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergyShare_v3.Infrastructure.Helpers
+{     /*Note : Ce helper est utilise par les behaviors pour savoir si le message en cours est une Command (ecriture) 
+       * ou une Query (lecture), afin d'appliquer un traitement conditionnel.*/
+    public static class TypeExtensions
+    {
+        public static bool IsCommand(this Type type)
+        {
+            if (type is null)
+                return false;
+
+            if (typeof(ICommand).IsAssignableFrom(type))
+                return true;
+
+            return type.GetInterfaces()
+                .Any(i => i.IsGenericType &&
+                           i.GetGenericTypeDefinition() == typeof(ICommand<>));
+        }
+
+        public static bool IsQuery(this Type type)
+        {
+            if (type is null)
+                return false;
+
+            return type.GetInterfaces()
+                .Any(i => i.IsGenericType &&
+                           i.GetGenericTypeDefinition() == typeof(IQuery<>));
+        }
+    }
+}
