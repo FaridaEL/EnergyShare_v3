90e837241c121d03d4df6898db43c3181ed35955
Farida EL KHABBABI <PSR12762@students.ephec.be>
2026-04-01 09:30:19 +0200

    Web : Endpoint, GlobalExceptionHandler

--- src/EnergyShare_v3.Web/Program.cs
+++ src/EnergyShare_v3.Web/Program.cs
@@ -3,6 +3,9 @@ using EnergyShare_v3.Application.Features.Users;
 using EnergyShare_v3.Infrastructure;
 using EnergyShare_v3.Infrastructure.Database;
 using EnergyShare_v3.Web.Components;
+using EnergyShare_v3.Web.Endpoints;
+using EnergyShare_v3.Web.Infrastructure;
+//using Microsoft.OpenApi;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +17,21 @@ builder.Services.AddRazorComponents()
 //Ajout l'infrastructure (EF Core, DbContext)
 // Un seul appel qui cache toute la complexite grace a la methode d'extension
 builder.Services.AddEnergyShare(builder.Configuration);
+
+
+// Exception handler
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+builder.Services.AddEndpointsApiExplorer();
+
+/*builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "EnergyShare API",
+        Version = "v1"
+    });
+});  */
 
 //Enregistrer les handlers de l'application 
 /*à vérifier ça me semble peu et contradiction entre le projet et les exos */
@@ -31,22 +49,33 @@ if (app.Environment.IsDevelopment())
     using var scope = app.Services.CreateScope();
     var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     await context.Database.EnsureCreatedAsync(); // Todo à remplacer par MigrateAsync() lorsque je fais mes migrations
+
+   // app.UseSwagger();
+   // app.UseSwaggerUI();
+
 }
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    //app.UseDeveloperExceptionPage();
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else { 
+    app.UseExceptionHandler();
+}
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+//Minimal API
+app.MapUsers();
 
 app.Run();
